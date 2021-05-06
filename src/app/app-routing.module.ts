cf5f20ceaa7fd911537b836fb96a3476cdf1d5b3
RemyMach <remygt@hotmail.fr>
2021-05-06 19:57:51 +0200

    add authenticate guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import {LoginComponent} from './components/login/login.component';
 import {HomeComponent} from './components/home/home.component';
 import {SubscribeComponent} from './components/subscribe/subscribe.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {AuthenticateGuard} from './guard/authenticate.guard';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent, canActivate: [AuthenticateGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'subscribe', component: SubscribeComponent},
   {path: '**', component: PageNotFoundComponent}
---
+++ src/app/guard/authenticate.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import {LocalStorageService} from '../services/local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateGuard implements CanActivate {
+
+  constructor(
+    private sessionService: LocalStorageService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if (this.sessionService.user === null) {
+      this.router.navigate(['login']);
+      return false;
+    }
+    return true;
+  }
+
+}
