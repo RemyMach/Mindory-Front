df20f34803228c2430357c1712f89f23b5dfc144
RemyMach <remygt@hotmail.fr>
2021-05-07 16:01:18 +0200

    add start of login and subscribe back

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  buttonIsInValidAfterClick = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  loginForm: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required, Validators.min(8), Validators.max(50)]
+  });
 
   ngOnInit(): void {
+  }
+
+  async postDisponibilityForm(): Promise<void> {
+    if (this.loginForm.invalid && !this.loginForm.dirty) {
+      return;
+    }
+
+    this.buttonIsInValidAfterClick = true;
+
+
+    await this.attemptToLogin();
+  }
+
+  private async attemptToLogin(): Promise<void> {
+
+    //tentative de connexion ici
   }
 
 }
--- src/app/components/subscribe/subscribe.component.ts
+++ src/app/components/subscribe/subscribe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-subscribe',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscribe.component.css']
 })
 export class SubscribeComponent implements OnInit {
+  buttonIsInValidAfterClick = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  subscribeForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    surname: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.min(8), Validators.max(50)]],
+    confirmationPassword: ['', [Validators.required]],
+    username: ['', [Validators.required]]
+  });
 
   ngOnInit(): void {
   }
 
+  async postDisponibilityForm(): Promise<void> {
+    if (this.subscribeForm.invalid && !this.subscribeForm.dirty) {
+      return;
+    }
+
+    this.buttonIsInValidAfterClick = true;
+
+
+    await this.attemptToLogin();
+  }
+
+  private async attemptToLogin(): Promise<void> {
+
+    /* TODO tentative d'inscription ici*/
+  }
 }
