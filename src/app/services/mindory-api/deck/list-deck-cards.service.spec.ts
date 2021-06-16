ccd5a99a31bd8b36cd865b9e706eb45bb3c09d79
RemyMach <remygt@hotmail.fr>
2021-06-16 18:33:53 +0200

    add chronometer to the play view, add banner to the view

---
+++ src/app/services/mindory-api/deck/list-deck-cards.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ListDeckCardsService } from './list-deck-cards.service';
+
+describe('ListDeckCardsService', () => {
+  let service: ListDeckCardsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ListDeckCardsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/services/mindory-api/deck/list-deck-cards.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {Deck} from '../../../models/deck.model';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {DefaultErrorService} from '../error/default-error.service';
+import {Observable} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListDeckCardsService {
+
+  public deck: Deck;
+  constructor(
+    private http: HttpClient,
+    private defaultErrorService: DefaultErrorService,
+  ) { }
+
+  private getDeckWithCards(): Observable<Deck> {
+    return this.http.get<Deck>(`http://localhost:3000/decks/play/1`).pipe(
+      tap(data => data),
+      catchError((err: HttpErrorResponse) => {
+        return this.defaultErrorService.handleError<Deck>(err, 'Incorrect request');
+      })
+    );
+  }
+  public getDeck(): void{
+    this.getDeckWithCards().subscribe(
+      value => this.deck = value,
+      err => console.log(err),
+      () => console.log('on a finit ici')
+    );
+  }
+}
