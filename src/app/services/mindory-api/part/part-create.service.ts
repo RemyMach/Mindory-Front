9513cfe8d444bec05b3bbbfd5f869646da3a6e8a
RemyMach <remygt@hotmail.fr>
2021-07-03 15:39:59 +0200

    add a feature to get deck easily with url and the request to create a part

---
+++ src/app/services/mindory-api/part/part-create.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {SessionModel} from '../../../models/session.model';
+import {catchError, tap} from 'rxjs/operators';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Card} from '../../../models/card.model';
+import {LocalStorageService} from '../../local-storage.service';
+import {DefaultErrorService} from '../error/default-error.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PartCreateService {
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  private baseUrl = 'http://localhost:3000/parts';
+
+  constructor(
+    private http: HttpClient,
+    private defaultErrorService: DefaultErrorService,
+  ) { }
+
+  public create(cards: Card[], deckId: number): Observable<any> {
+    const cardIds: number[] = cards.map(card => card.id);
+    return this.http.post<SessionModel>(`${this.baseUrl}/`, {cardIds, deckId}, this.httpOptions)
+      .pipe(
+        tap(data => {
+          return;
+        }),
+        catchError((err: HttpErrorResponse) => {
+          return this.defaultErrorService.handleError<string>(err, 'Please retry later');
+        })
+      );
+  }
+}
