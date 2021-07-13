59c428f0b34a0b92d8ace2f9aad9a70b5fd11f83
RemyMach <remygt@hotmail.fr>
2021-07-13 16:39:31 +0200

    add the possibility to see a refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {NavigationStart, Router} from '@angular/router';
+
+export let browserRefresh = false;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   title = 'Mindory-Front';
+  subscription: Subscription;
+
+  constructor(private router: Router) {
+    this.subscription = router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        browserRefresh = !router.navigated;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {LocalStorageService} from '../../services/local-storage.service';
+import {NavigationStart, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+
+import { browserRefresh } from '../../app.component';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +12,18 @@ import {LocalStorageService} from '../../services/local-storage.service';
 })
 export class HomeComponent implements OnInit {
 
+  public browserRefresh: boolean;
+
   constructor(
-    private localStorageService: LocalStorageService
-  ) { }
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {
+  }
 
   ngOnInit(): void {
     this.localStorageService.deleteParamGame();
+    this.browserRefresh = browserRefresh;
+    console.log(browserRefresh);
   }
 
 }
