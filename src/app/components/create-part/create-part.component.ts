e2f51faef9acec41ee64c42a178e22a787846cd6
RemyMach <remygt@hotmail.fr>
2021-07-16 12:01:15 +0200

    refacto view for the creation of a part

--- src/app/components/create-part/create-part.component.ts
+++ src/app/components/create-part/create-part.component.ts
@@ -3,6 +3,7 @@ import {PlayDuoService} from '../../services/play/play-duo.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {SnackbarService} from '../../services/snackbar.service';
 import {Router} from '@angular/router';
+import {LocalStorageService} from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-create-part',
@@ -11,11 +12,14 @@ import {Router} from '@angular/router';
 })
 export class CreatePartComponent implements OnInit {
 
+  informationSentence: string;
+
   constructor(
     public playDuoService: PlayDuoService,
     private formBuilder: FormBuilder,
     private snackBar: SnackbarService,
-    public router: Router
+    public router: Router,
+    public localStorageService: LocalStorageService
   ) { }
 
   createKeyWordForm: FormGroup = this.formBuilder.group({
@@ -24,6 +28,7 @@ export class CreatePartComponent implements OnInit {
 
   ngOnInit(): void {
     this.playDuoService.getActualRoomWithoutSocket();
+    this.formatInformationSentence();
   }
 
 
@@ -33,6 +38,13 @@ export class CreatePartComponent implements OnInit {
     }
     this.playDuoService.buttonCreatePart = true;
     this.playDuoService.createKeywordForARoom(this.createKeyWordForm.get('keyWord').value);
+  }
+
+  private formatInformationSentence(): void {
+    if(this.localStorageService.getSessionToken())
+      this.informationSentence = 'Vous pouvez choisir d\'envoyez l\'URL ci-dessous à l\'autre personne ou de générer un mot de passe qu\'elle pourra rentréelle même dans la partie rejoindre partie';
+    else
+      this.informationSentence = 'Vous pouvez envoyez cette Url à l\'autre personne';
   }
 
 }
